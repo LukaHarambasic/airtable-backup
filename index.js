f3739701c50ace4c9df5679059ffa8a6ac627cce
LukaHarambasic <luka@harambasic.de>
2022-10-10 22:57:22 +0200

    convert table to csv based on static input

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 require('dotenv').config()
+const Axios = require('axios')
 
-const { AIRTABLE_API_KEY: API_KEY, AIRTABLE_BASE_IDS } = process.env
+const { AIRTABLE_API_KEY: API_KEY, TEST_AIRTABLE_BASE_ID: BASE_ID } = process.env
 
-const BASE_IDS = AIRTABLE_BASE_IDS.split(',').map(base => {
-    const [display, id] = base.split(':')
-    return {
-        display,
-        id
+async function getTable(baseId, table) {
+    const REQUEST_URL = `https://api.airtable.com/v0/${baseId}/${table}`
+    const config = {
+        headers: { Authorization: `Bearer ${API_KEY}` }
     }
-})
+    const {data: {records}} = await Axios.get(REQUEST_URL, config)
+    return records
+}
 
-console.log(API_KEY)
-console.log(BASE_IDS)
+async function tableToCSV(table) {
+    const content = table.map(row => {
+        const fields = Object.values(row.fields).join(';')
+        return `${row.id};${row.createdTime};${fields}`
+    })
+    const fieldNames = Object.keys(table[0].fields).join(';')
+    const header = `ID;Created Time;${fieldNames}`
+    return `${header}\n${content.join('\n')}`
+}
+
+async function main() {
+    // TODO make all of this dynamic - waiting for access
+    // base id & table will be fetched via meta data after I got access
+    // https://airtable.com/api/meta
+    const table = await getTable(BASE_ID, 'Main')
+    const csv = await tableToCSV(table)
+    console.log(csv)
+}
+
+main ()
